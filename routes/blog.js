b2e68b3b8b9f0123669273c3677ac21f1c00fb16
TusharChhabra658 <tusharchhabra658@gmail.com>
2025-05-06 18:08:07 +0530

    update

--- routes/blog.js
+++ routes/blog.js
@@ -3,6 +3,7 @@ const multer = require("multer");
 const path = require("path");
 const router = Router();
 const Blog = require("../models/blog");
+const Comment = require("../models/comment");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -24,15 +25,28 @@ router.get("/add-new", (req, res) => {
 
 router.get("/:id", async (req, res) => {
   const blog = await Blog.findById(req.params.id).populate("createdBy");
+  const comments = await Comment.find({ blogId: req.params.id }).populate(
+    "createdBy"
+  );
+  console.log(comments);
   return res.render("blog", {
     user: req.user,
     blog,
+    comments,
   });
 });
+
+router.post("/comment/:blogId", async (req, res) => {
+  const comment = await Comment.create({
+    content: req.body.content,
+    createdBy: req.user._id,
+    blogId: req.params.blogId,
+  });
+  return res.redirect(`/blog/${req.params.blogId}`);
+});
+
 router.post("/", upload.single("coverImage"), async (req, res) => {
   const { title, body } = req.body;
-  // console.log(req.body);
-  // console.log(req.file);
   const blog = await Blog.create({
     title,
     body,
